8bad0c90e5f4f193fd164d0e04754405bcbd7352
Helen <helen.youchen@gmail.com>
2024-01-16 18:05:06 +0800

    加入專案檔案。

---
+++ LayoutTry/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LayoutTry.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
